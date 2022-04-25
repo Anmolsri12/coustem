db4480e9d0bc251ade4136379ce68d343c177941
Anmolsri12 <anmolsrivastava60669@gmail.com>
2022-04-25 12:55:20 +0530

    kkk

---
+++ lib/Anmol.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Anmol extends StatelessWidget {
+  const Anmol({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    padding:const EdgeInsets.all(12.0),
+    child:const Text("My name Anmol Srivastava"),);
+  }
+}
